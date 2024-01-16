6e73bba1ff29741748c0306e751fe8d8bf2323e5
Giorgi Nareklishvili <38813634+gionare@users.noreply.github.com>
2024-01-15 23:32:29 -0500

    import fonts from https://fontsource.org/fonts/firago/install added for newBlog page

--- pages/NewBlog/newBlog.js
+++ pages/NewBlog/newBlog.js
@@ -1,3 +1,7 @@
-let today = new Date().toISOString().split("T")[0];
+import "@fontsource/firago/400.css";
+import "@fontsource/firago/500.css";
+import "@fontsource/firago/600.css";
 
+// actual date placeholder for publish date input
+let today = new Date().toISOString().split("T")[0];
 document.getElementById("todayDate").setAttribute("value", today);
