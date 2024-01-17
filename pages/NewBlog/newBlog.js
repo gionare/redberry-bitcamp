1b08498eaccced959c25589d39aba063244d41db
Giorgi Nareklishvili <38813634+gionare@users.noreply.github.com>
2024-01-17 14:07:20 -0500

    newBlog page is alsmost done

--- pages/NewBlog/newBlog.js
+++ pages/NewBlog/newBlog.js
@@ -2,6 +2,11 @@ import "@fontsource/firago/400.css";
 import "@fontsource/firago/500.css";
 import "@fontsource/firago/600.css";
 
+// js to trigger the file input when the text is clicked
+function triggerFileInput() {
+  document.getElementById("img").click();
+}
+
 // actual date placeholder for publish date input
 let today = new Date().toISOString().split("T")[0];
 document.getElementById("todayDate").setAttribute("value", today);
